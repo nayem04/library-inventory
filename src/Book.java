0dbcd86b56764c14724d824a839dd492e6ce4b16
Mohammad Nayem <sheikhmdnayem@gmail.com>
2024-12-26 13:24:03 +0600

    Added Book Class

---
+++ src/Book.java
@@ -0,0 +1,63 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String genre;
+
+    public Book() {}
+
+    public Book(int id, String title, String author, int publicationYear, String genre) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book ID: %d, Title: %s, Author: %s, Year: %d, Genre: %s",
+                id, title, author, publicationYear, genre);
+    }
+}
