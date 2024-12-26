c36be281cc919026059fecbdd442e3148316cf04
Mohammad Nayem <sheikhmdnayem@gmail.com>
2024-12-26 17:47:44 +0600

    Added function for searching books

--- src/Library.java
+++ src/Library.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Library {
     public static Library instance;
     private final List<Book> books;
 
     private Library() {
-        books = new ArrayList<>();
+//        books = new ArrayList<>();
 
-        /*books = Arrays.asList(new Book(1, "Debi", "author", 2014, "genre"),
+        books = Arrays.asList(new Book(1, "Debi", "author", 2014, "genre"),
                 new Book(2, "Amar Bondhu Rashed", "author", 2005, "genre"),
-                new Book(3, "Dipu Number 2", "author", 2000, "genre"));*/
+                new Book(3, "Dipu Number 2", "author", 2000, "genre"));
 
     }
 
@@ -21,5 +24,14 @@ public class Library {
 
     public List<Book> getBooks() {
         return books;
+    }
+
+    public List<Book> searchBooks(String title, String author, Integer year) {
+        if (title.isBlank() && author.isBlank() && year == null)
+            return new ArrayList<>();
+        Stream<Book> bookStream = (year == null) ?
+                books.stream().filter(b -> b.getTitle().equalsIgnoreCase(title) || b.getAuthor().equalsIgnoreCase(author)) :
+                books.stream().filter(b -> b.getTitle().equalsIgnoreCase(title) || b.getAuthor().equalsIgnoreCase(author) || b.getPublicationYear() == year);
+        return bookStream.collect(Collectors.toList());
     }
 }
--- src/LibraryInventory.java
+++ src/LibraryInventory.java
@@ -4,19 +4,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class LibraryInventory {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Library library = Library.getInstance();
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Library Inventory Management System.");
 
-        Scanner scanner = new Scanner(System.in);
         boolean exit = false;
-        Library library = Library.getInstance();
-
         while (!exit) {
             System.out.println("""
                     \nPlease enter a number between 1 to 5 to choose an option
                     1. Display Books
-                    2. Add A New Book
-                    3. Search For A Book
+                    2. Search For Books
+                    3. Add A New Book
                     4. Remove A Book
                     5. Exit The Program
                     """);
@@ -32,12 +32,9 @@ public class LibraryInventory {
             }
 
             switch (option) {
-                case 1 -> {
-                    System.out.println("Display Books");
-                    displayBooks(library);
-                }
-                case 2 -> System.out.println("Add A New Book");
-                case 3 -> System.out.println("Search For A Book");
+                case 1 -> displayBooks();
+                case 2 -> searchBooks();
+                case 3 -> System.out.println("Add A New Book");
                 case 4 -> System.out.println("Remove A Book");
                 case 5 -> exit = true;
                 default -> System.out.println("Invalid option. Please try again.");
@@ -47,7 +44,8 @@ public class LibraryInventory {
         scanner.close();
     }
 
-    private static void displayBooks(Library library) {
+    private static void displayBooks() {
+        System.out.println("Display Books");
         List<Book> books = library.getBooks();
         if (books.isEmpty()) {
             System.out.println("There are no books in the library.");
@@ -56,5 +54,28 @@ public class LibraryInventory {
                     .sorted(Comparator.comparing(Book::getTitle))
                     .forEach(System.out::println);
         }
+    }
+
+    private static void searchBooks() {
+        System.out.println("Search For Books");
+        System.out.print("Enter Title or enter: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter Author or enter: ");
+        String author = scanner.nextLine();
+
+        System.out.print("Enter Publication Year or enter: ");
+        String yearString = scanner.nextLine();
+        Integer year;
+        try {
+            year = (yearString.isBlank()) ? null: Integer.parseInt(yearString);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid year. Please try again.");
+            return;
+        }
+
+        List<Book> books = library.searchBooks(title, author, year);
+        if (books.isEmpty()) System.out.println("No books found.");
+        else books.forEach(System.out::println);
     }
 }
